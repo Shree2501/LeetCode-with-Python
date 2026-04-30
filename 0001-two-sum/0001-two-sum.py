893541933fe32fc5f83aa4a49ce6bfbaab0fcf80
Shree Sharma <sharmashree750@gmail.com>
2026-04-30 20:45:43 +0530

    Time: 3 ms (54.37%), Space: 13.3 MB (16.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        res = []
+        freq = {}
         n = len(nums)
-        for i in range(0,n-1):
-            sum = 0
-            for j in range(i+1,n):
-                sum = nums[i] + nums[j]
-                if(sum == target):
-                    return i,j
-        
+        for i in range(n):
+            val = target - nums[i]
+            if val in freq:
+                return freq[val],i
+            freq[nums[i]] = i
